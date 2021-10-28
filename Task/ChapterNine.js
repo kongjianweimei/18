9f10c5478e647dfc9c80ae5537646b23f78186b0
kongjianweimei <79363524+kongjianweimei@users.noreply.github.com>
2021-10-28 18:22:45 +0800

    Update ChapterNine.js

--- Task/ChapterNine.js
+++ Task/ChapterNine.js
@@ -5,7 +5,7 @@ const $ = Env(jobname)
 
 let taskmap = new Map();
 let articleidarr = [];
-let TokenArr = ['aXx-kVULBnI_wWQi_YcRvwdsQqcJarlW', ''];
+let TokenArr = ['aXx-kVULBnI_wWQi_YcRvwdsQqcJarlW', 'ULN6oXzNh8Ha2oBejnBplModwG92UbUp'];
 !(async () => {
     await all();
 })()
