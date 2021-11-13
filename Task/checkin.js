70f449ce8ce2ee11ae2a5f059022612cef1bdeb2
kongjianweimei <79363524+kongjianweimei@users.noreply.github.com>
2021-11-13 23:46:32 +0800

    Update checkin.js

--- Task/checkin.js
+++ Task/checkin.js
@@ -21,7 +21,7 @@ const crypto = require('crypto');
 async function all() {
 
   $.log("【九章头条提现】");
-  let TokenArr = ['h7qU2t9heS1G_SRSGdKUn4_nDTmtppE0','TUytnqAHRy2Dn9VJ-3yl3XyC-46RteN8','fEWNAMgcSnrlcw3OToHEj2vx3-2xiX8C','x1YmVf_xtFu4zqUFzhq3m8BPw1MuimS7 ','opscEfmu0-ZYMPM0MH3fqY-oj4wnp6F9','56cN_cDoo8PfEr2PP85yCOK7FPt-NZH7', 'aXx-kVULBnI_wWQi_YcRvwdsQqcJarlW'];
+  let TokenArr = ['YEAHkBqey1jDt-evJtrjvlTD2XiMT4Fz','TUytnqAHRy2Dn9VJ-3yl3XyC-46RteN8','fEWNAMgcSnrlcw3OToHEj2vx3-2xiX8C','x1YmVf_xtFu4zqUFzhq3m8BPw1MuimS7 ','opscEfmu0-ZYMPM0MH3fqY-oj4wnp6F9','56cN_cDoo8PfEr2PP85yCOK7FPt-NZH7', 'aXx-kVULBnI_wWQi_YcRvwdsQqcJarlW'];
 
   for (let i = 0; i < TokenArr.length; i++) {
     $.log(`开始执行第${i + 1}个账号`);
