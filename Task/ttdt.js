7c6b9b38bfbda6e334a169224ed02c39bf071363
kongjianweimei <79363524+kongjianweimei@users.noreply.github.com>
2021-12-23 17:59:51 +0800

    Update ttdt.js

--- Task/ttdt.js
+++ Task/ttdt.js
@@ -20,8 +20,8 @@ let index
 $.message = "";
 imeiarr = [
    
-    "202118AC-3BEB-45E0-80CB-69C7E5EFCB86",
-    "810ECFA0-970A-4EFF-92B4-8223A4F33209"
+    "97EB9204-DF98-4FD8-9492-DD95AA83223B",
+    ""
 ]
 !(async () => {
     await all();
